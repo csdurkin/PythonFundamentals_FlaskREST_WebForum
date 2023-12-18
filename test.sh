7e7e9efa25598386ce7e9d47497d853fd31d2134
csdurkin <151471312+csdurkin@users.noreply.github.com>
2023-12-17 23:09:01 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status.
-# This ensures that any errors during script execution will cause it to terminate immediately.
 set -e
 
-# Define a trap to ensure that the Flask application is stopped when the script exits.
 # The trap command will execute the 'kill $PID' command when the script exits.
 trap 'kill $PID' EXIT # Kill the server on exit
 
 # Start your Flask application using your script (e.g., run.sh) in the background.
-# The '&' symbol runs the script in the background.
 ./run.sh &
 
 # Record the Process ID (PID) of the Flask application.
-# This allows us to track the background process.
 PID=$! # Record the PID
 
 # Use Newman (a Postman command-line tool) to run API tests from "forum_multiple_posts.postman_collection.json."
