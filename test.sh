93a4ce8b188efe5fa72843e8edda27aaad5bdc17
csdurkin <151471312+csdurkin@users.noreply.github.com>
2023-12-17 23:56:33 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,12 +12,10 @@ trap 'kill $PID' EXIT # Kill the server on exit
 # Record the Process ID (PID) of the Flask application.
 PID=$! # Record the PID
 
-# Use Newman (a Postman command-line tool) to run API tests from "forum_multiple_posts.postman_collection.json."
+# Use Newman (a Postman command-line tool) to run API tests from "cdurkin_cs515_project3_collection.postman_collection.json."
 # The '-e' flag specifies the environment file to use.
-newman run cdurkin_cs515_project3_collection.postman_collection.json -e env.json # Use the env file
+newman run cdurkin_cs515_project3_collection.postman_collection.json -e cdurkin_project3_enviroment.postman_environment.json # Use the specified environment file
 
 # Run Newman to execute the API tests defined in "forum_post_read_delete.postman_collection.json" with 50 iterations per test.
 # The '-n' flag specifies the number of iterations.
 newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
-
-
